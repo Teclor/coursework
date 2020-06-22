8830684192b491a99c8695c6a29a6a4b26bb4717
Teclor <teclorian@gmail.com>
2020-06-22 14:43:30 +0400

    comboboxes and interface realisation

--- main.py
+++ main.py
@@ -5,23 +5,39 @@ import translator
 
 DICTIONARY = "rus_dict"
 
+
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.QuitBtn.clicked.connect(QtWidgets.QApplication.instance().quit)
         self.ui.translateBtn.clicked.connect(self.translate)
+        self.ui.output.setReadOnly(True)
+        self.ui.input.setPlaceholderText("Введите слова для перевода")
+        self.ui.output.setPlaceholderText("Перевод")
+        self.ui.langFrom.currentIndexChanged.connect(self.change_lang)
+        self.ui.langTo.currentIndexChanged.connect(self.change_lang)
         
+
     def translate(self):
+        en = True if (self.ui.langTo.currentIndex() == 1) else False
         tr = translator.Translator(DICTIONARY)
-        in_text = self.ui.input.text()
+        in_text = self.ui.input.toPlainText()
         if in_text:
-            out_text = tr.translate(str(in_text))
+            out_text = tr.translate(str(in_text), en)
             if out_text:
-                self.ui.output.setText("Перевод слова: " + str(out_text))
+                self.ui.output.setPlainText(str(out_text))
             else:
-                self.ui.output.setText("Перевод не найден.\nПожалуйста, проверьте введённые данные и попробуйте ещё раз.")
+                self.ui.output.setPlainText("Перевод не найден.\nПожалуйста, проверьте введённые данные и попробуйте ещё раз.")
+
+    def change_lang(self, index):
+        sender = self.sender()
+        from_combo = self.ui.langFrom
+        to_combo = self.ui.langTo
+        if sender == from_combo and to_combo != index:
+            to_combo.setCurrentIndex(index)
+        if sender == to_combo and from_combo != index:
+            from_combo.setCurrentIndex(index)
 
 
 app = QtWidgets.QApplication([])
