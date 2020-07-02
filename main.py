8662396cd94ba1b25d38e39bcf7147a479a42fcf
Antony <teclorian@gmail.com>
2020-07-03 01:07:09 +0400

    modal windows in dev

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QFileInfo, QThread
+from PyQt5.QtWidgets import QFileDialog, QDialog, QComboBox
+
 from interface import Ui_MainWindow
 import sys
 import translator
 
-DICTIONARY = "rus_dict"
+
+class TryThread(QThread):
+
+  def new_run(self):
+    print("Abstract class")
+
+  def run(self):
+    try:
+      self.new_run()
+    except:
+      print ("Ups")
+
+class PreDialog(QDialog):
+    def __init__(self, parent=None):                      # + parent
+        super(PreDialog, self).__init__(parent)
+        combo = QComboBox(self)
+        combo.addItem("Apple")
+        combo.addItem("Pineapple")
+        self.setWindowTitle('Host Parameters')
+        self.setModal(True)
+        self.line_host = QtWidgets.QLineEdit()
+        self.line_user = QtWidgets.QLineEdit()
+        self.line_pass = QtWidgets.QLineEdit()
+        self.line_pass.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.save = QtWidgets.QPushButton("Сохранить")
+        self.spinBox_port = QtWidgets.QSpinBox()
+        self.spinBox_port.setProperty("value", 22)
+        self.hbox = QtWidgets.QHBoxLayout()
+        self.ssh_radio = QtWidgets.QRadioButton("SSH")
+        self.telnet_radio = QtWidgets.QRadioButton("Telnet")
+
+        self.hbox.addWidget(self.ssh_radio)
+        self.hbox.addWidget(self.telnet_radio)
+
+        self.form = QtWidgets.QFormLayout()
+        self.form.setSpacing(20)
+
+        self.form.addRow("&Язык:", combo)
+        self.form.addRow("&Host:",self.line_host)
+        self.form.addRow("&User:",self.line_user)
+        self.form.addRow("&Password:",self.line_pass)
+        self.form.addRow("&Port:",self.spinBox_port)
+        self.form.addRow("Session:",self.hbox)
+        self.form.addRow(self.save)
+
+        self.setLayout(self.form)
 
 
 class MyWindow(QtWidgets.QMainWindow):
@@ -17,11 +65,12 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.output.setPlaceholderText("Перевод")
         self.ui.langFrom.currentIndexChanged.connect(self.change_lang)
         self.ui.langTo.currentIndexChanged.connect(self.change_lang)
-        
+        self.ui.dictionaries.triggered.connect(self.show_dictionaries_menu)
 
     def translate(self):
+        settings = translator.Settings()
         en = True if (self.ui.langTo.currentIndex() == 1) else False
-        tr = translator.Translator(DICTIONARY)
+        tr = translator.Translator(translator.Settings.get_option())
         in_text = self.ui.input.toPlainText()
         if in_text:
             out_text = tr.translate(str(in_text), en)
@@ -39,9 +88,25 @@ class MyWindow(QtWidgets.QMainWindow):
         if sender == to_combo and from_combo != index:
             from_combo.setCurrentIndex(index)
 
+    def show_open_file_dialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Откройте файл со словарем', '/', "Файлы XML-словаря (*.xdxf);;Текстовые файлы (*.txt)")[0]
+        if fname:
+            filename = QFileInfo(fname).baseName()
+            f = open(fname, 'r')
+            with f:
+                data = f.read()
+                #textEdit.setText(DICTIONARY)
+
+    def show_error(self, err):
+        error_dialog = QtWidgets.QErrorMessage()
+        error_dialog.showMessage("Ошибка: " + str(err))
+
+    def show_dictionaries_menu(self):
+        dialog = PreDialog(self.ui.centralWidget)
+        dialog.show()
 
 app = QtWidgets.QApplication([])
 application = MyWindow()
 application.show()
-
 sys.exit(app.exec())
+
--- translator.py
+++ translator.py
@@ -1,77 +1,155 @@
 import sqlite3 as sql
 import re
 
+from PyQt5.QtCore import QCoreApplication, QSettings
+from PyQt5 import QtWidgets
+try:
+    class DB:
 
-class DB:
+        def __init__(self, current_dictionary, name="dictionary"):
+            db_path = "resource\\db\\" + name + ".db"
+            self.conn = sql.connect(db_path)
+            self.DICT = current_dictionary
 
-    def __init__(self, current_dictionary, name="dictionary"):
-        db_path = "resource\\db\\" + name + ".db"
-        self.conn = sql.connect(db_path)
-        self.DICT = current_dictionary
+        def create_table(self):
+            db = self.conn.cursor()
+            db.execute("CREATE TABLE " + self.DICT + " (word text NOT NULL, translation text NOT NULL)")
 
-    def create_table(self):
-        db = self.conn.cursor()
-        db.execute("CREATE TABLE " + self.DICT + " (word text NOT NULL, translation text NOT NULL)")
+        def insert_translation(self, word, translation):
+            db = self.conn.cursor()
+            cmd = "INSERT INTO {t} (word, translation) VALUES (?,?)"
+            cmd = cmd.format(t=self.DICT)
+            db.execute(cmd, (word, translation))
 
-    def insert_translation(self, word, translation):
-        db = self.conn.cursor()
-        cmd = "INSERT INTO {t} (word, translation) VALUES (?,?)"
-        cmd = cmd.format(t=self.DICT)
-        db.execute(cmd, (word, translation))
+        def search_translation(self, word):
+            db = self.conn.cursor()
+            cmd = "SELECT translation FROM {table} WHERE word LIKE ?"
+            cmd = cmd.format(table=self.DICT)
+            db.execute(cmd, (word.lower(),))
+            result = db.fetchall()
+            if result:
+                return str((result[0][0]))
+            else:
+                return False
 
-    def search_translation(self, word):
-        db = self.conn.cursor()
-        cmd = "SELECT translation FROM {table} WHERE word LIKE ?"
-        cmd = cmd.format(table=self.DICT)
-        db.execute(cmd, (word.lower(),))
-        result = db.fetchall()
-        if result:
-            return str((result[0][0]))
-        else:
-            return False
-
-    def __del__(self):
-        self.conn.close()
-
-
-class Parser(DB):
-
-    def parse_xdxf(self):
-        with open('resource/raw/' + self.DICT + '.xdxf', encoding="utf-8") as fin:
-            text = fin.read()
-        self.create_table()
-        match = re.findall('<ar>.*?</ar>', text, flags=re.DOTALL)  # ленивый квантификатор
-        for m in match:
-            m = re.sub("<ar>|</ar>|<k>", "", m)
-            m = re.split(r"</k>\n", m)
-            if m[0] and m[1]:
-                self.insert_translation(m[0], m[1])
-        self.conn.commit()
+        def __del__(self):
+            self.conn.close()
 
 
-class Translator(DB):
+    class Parser(DB):
 
-    def translate(self, text, en=True):
-        check = re.findall(r"\b(?:\w+-?)\b", text, flags=re.DOTALL)
-        cnt = len(check)
-        if cnt > 1:
-            return self.translate_text(text, en)
-        elif cnt == 1:
-            return self.translate_word(text)
+        def parse_xdxf(self):
+            with open('resource/raw/' + self.DICT + '.xdxf', encoding="utf-8") as fin:
+                text = fin.read()
+            self.create_table()
+            match = re.findall('<ar>.*?</ar>', text, flags=re.DOTALL)  # ленивый квантификатор
+            for m in match:
+                m = re.sub("<ar>|</ar>|<k>", "", m)
+                m = re.split(r"</k>\n", m)
+                if m[0] and m[1]:
+                    self.insert_translation(m[0], m[1])
+            self.conn.commit()
 
-    def translate_word(self, word):
-        return self.search_translation(word)
+        def parse_txt(self):
+            with open('resource/raw/' + self.DICT + '.txt', encoding="utf-8") as fin:
+                text = fin.read()
+            self.create_table()
+            match = re.findall('<ar>.*?</ar>', text, flags=re.DOTALL)  # ленивый квантификатор
+            for m in match:
+                m = re.sub("<ar>|</ar>|<k>", "", m)
+                m = re.split(r"</k>\n", m)
+                if m[0] and m[1]:
+                    self.insert_translation(m[0], m[1])
+            self.conn.commit()
 
-    def translate_text(self, text, en):
-        match = re.findall(r"\b(?:\w+-?)\b", text, flags=re.DOTALL)
-        for m in match:
-            tr = self.search_translation(m)
-            if tr:
-                if en:
-                    reg = r"\b(?:[A-Za-z']+-?\.?\s?)+\b"
-                else:
-                    reg = r"\b(?:[А-Яа-я']+-?\.?\s?)+\b"
-                tr = re.search(reg, tr, flags=re.IGNORECASE)
-                if tr[0]:
-                    text = re.sub(m, tr[0].strip(), text)
-        return text
+
+    class Translator(DB):
+
+        def translate(self, text, en=True):
+            raise Exception("hmmm")
+            check = re.findall(r"\b(?:\w+-?)\b", text, flags=re.DOTALL)
+            cnt = len(check)
+            if cnt > 1:
+                return self.translate_text(text, en)
+            elif cnt == 1:
+                return self.translate_word(text)
+
+        def translate_word(self, word):
+            return self.search_translation(word)
+
+        def translate_text(self, text, en):
+            match = re.findall(r"\b(?:\w+-?)\b", text, flags=re.DOTALL)
+            for m in match:
+                tr = self.search_translation(m)
+                if tr:
+                    if en:
+                        reg = r"\b(?:[A-Za-z']+-?\.?\s?)+\b"
+                    else:
+                        reg = r"\b(?:[А-Яа-я']+-?\.?\s?)+\b"
+                    tr = re.search(reg, tr, flags=re.IGNORECASE)
+                    if tr[0]:
+                        text = re.sub(m, tr[0].strip(), text)
+            return text
+
+
+    """
+    Class that is a wrapper on QSettings
+    Used for more simple settings manipulating
+    Singleton
+    """
+
+
+    class Settings:
+
+        def __new__(cls):
+            """
+            Checks the existence of class instance
+            :return: instance of Settings
+            """
+            if not hasattr(cls, 'instance'):
+                cls.instance = super(Settings, cls).__new__(cls)
+            return cls.instance
+
+        def __init__(self):
+            """
+            Some default app parameters
+            """
+            self.version = "1.0.0"
+            self.organization = "AB"
+            self.application = "STranslate"
+            if not QCoreApplication.organizationName() or not QCoreApplication.applicationName():
+                self.set_default_options()
+            self.settings = QSettings(QCoreApplication.organizationName(), QCoreApplication.applicationName())
+
+        def set_default_options(self):
+            """
+            Sets default settings
+            """
+            QCoreApplication.setApplicationVersion(self.version)
+            QCoreApplication.setOrganizationName(self.organization)
+            QCoreApplication.setApplicationName(self.application)
+            settings = QSettings(QCoreApplication.organizationName(), QCoreApplication.applicationName())
+            settings.setValue('dictionary/from', 'rus_eng')
+            settings.setValue('dictionary/to', 'eng_rus')
+            settings.setValue('language/from', 'ru')
+            settings.setValue('language/to', 'en')
+
+        def set_option(self, option, value, default=False):
+            """
+            :param option: string name of the option to set
+            :param value: string value of the option
+            :param default: bool true if you want to discard all options
+            """
+            if not default:
+                self.settings.setValue(option, value)
+            else:
+                self.set_default_options()
+
+        def get_option(self, option):
+            return self.settings.value(option)
+
+
+
+except Exception as e:
+    error_dialog = QtWidgets.QErrorMessage()
+    error_dialog.showMessage("Ошибка: " + str(e))
